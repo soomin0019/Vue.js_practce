715b25316924ba568e24ff29b840501abb9ee5e1
이수민 <Smlee0109@naver.com>
2023-08-18 12:09:18 +0900

    feat(230818) : TodoList만들기(ch2_8)

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form 
+    @submit.prevent="onSubmit" 
+    class="d-flex">
+      <div class="flex-grow-1 mr-2">
+        <input 
+        class="form-control"
+        type="type" 
+        v-model="todo"
+        placeholder="Type new to-do"
+        >
+      </div>
+      <div>
+        <button 
+        class="btn btn-primary"
+        type="submit"
+        @click="onSubmit"
+      >Add</button> 
+      </div>
+
+    </form>
+    {{ todos }}
+    <div class="card">
+      <div class="card-body p-2">
+        {{ todos[0] }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const todo = ref('');
+    const todos = ref([
+      {id : 1, subject: '휴대폰 사기'},
+      {id : 2, subject: '장보기'}
+    ]);
+    
+    const onSubmit = () => {
+      todos.value.push({
+        id : Date.now(),
+        subject : todo.value
+      });
+    };
+
+    
+    return {
+      todo,
+      todos,
+      onSubmit,
+      
+    };
+  }
+}
+</script>
+
+<style>
+.name {
+  color : red;
+}
+</style>
